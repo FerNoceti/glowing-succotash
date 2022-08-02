f530df5df481c9d99f1baf19feb9ff11c18fcf09
FerNoceti <fer.j.noceti@gmail.com>
2022-08-02 16:25:13 -0300

    Update Noise.cs

--- Game/Assets/Scripts/Enviroment/Noise.cs
+++ Game/Assets/Scripts/Enviroment/Noise.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public static class Noise {
 
 	public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, int seed, float scale, int octaves, float persistance, float lacunarity, Vector2 offset) {
+		
 		float[,] noiseMap = new float[mapWidth,mapHeight];
 
 		System.Random prng = new System.Random (seed);
